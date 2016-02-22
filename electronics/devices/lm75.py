21caa9e3768efb74f726706599af4ea95cf70da7
Martijn Braam <martijn@brixit.nl>
2016-02-22 21:19:46 +0100

    Added docs to LM75

--- electronics/devices/lm75.py
+++ electronics/devices/lm75.py
@@ -3,10 +3,32 @@ import struct
 
 
 class LM75(I2CDevice):
+    """
+    Interface to any LM75 compatible temperature sensor (Used NXP LM75A for testting)
+
+    :Usage:
+
+    * Use temperature() to get the temperature in degree celcius
+
+    .. testsetup::
+
+        from electronics.gateways import MockGateway
+        from electronics.devices import LM75
+        gw = MockGateway()
+
+    :Example:
+
+    >>> sensor = LM75(gw)
+    >>> sensor.temperature()
+    37.29
+    """
+
     def __init__(self, bus, address=0x49):
         super().__init__(bus, address)
 
     def temperature(self):
+        """ Get the temperature in degree celcius
+        """
         result = self.i2c_read(2)
         value = struct.unpack('>H', result)[0]
 
