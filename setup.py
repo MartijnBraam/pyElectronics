d90f4d7688887101f1e2d52b8ce01239323b866f
Martijn Braam <martijn@brixit.nl>
2016-02-23 12:39:30 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
         name='pyelectronics',
-        version='0.1.1',
+        version='0.1.2',
         packages=['electronics', 'electronics.devices', 'electronics.gateways'],
         url='https://github.com/MartijnBraam/pyElectronics',
         license='MIT',
