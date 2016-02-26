277de97279658e2d8ec494317c4300e095dbc7cd
Martijn Braam <martijn@brixit.nl>
2016-02-26 21:01:28 +0100

    Added letters to 7 segment class

--- electronics/devices/sevensegment.py
+++ electronics/devices/sevensegment.py
@@ -1,4 +1,5 @@
 from electronics.device import GPIODevice
+from electronics.gpio import GPIOBus
 
 
 class SevenSegmentGPIO(GPIODevice):
@@ -17,14 +18,43 @@ class SevenSegmentGPIO(GPIODevice):
         '6': 0b01111011,
         '7': 0b00100111,
         '8': 0b01111111,
-        '9': 0b01101111
+        '9': 0b01101111,
+        'a': 0b00111111,
+        'b': 0b01111010,
+        'c': 0b01010011,
+        'd': 0b01111100,
+        'e': 0b01011011,
+        'f': 0b00011011,
+        'g': 0b01101111,
+        'h': 0b00111110,
+        'i': 0b00010010,
+        'j': 0b01110100,
+        'k': 0b00111110,
+        'l': 0b01010010,
+        'm': 0b00110001,
+        'n': 0b00111000,
+        'o': 0b01110111,
+        'p': 0b00011111,
+        'q': 0b00101111,
+        'r': 0b00011000,
+        's': 0b01101011,
+        't': 0b01011010,
+        'u': 0b01110110,
+        'v': 0b01110000,
+        'w': 0b01000110,
+        'x': 0b00111110,
+        'y': 0b00001110,
+        'z': 0b01011101,
+        ' ': 0b00000000
     }
 
     def __init__(self, segments):
         """
-        :param segments: GPIOBus instance with the pins
-        :return:
+        :param segments: GPIOBus instance with the pins or array of pins
         """
+
+        if isinstance(segments, list):
+            segments = GPIOBus(segments)
         self.segments = segments
 
     def write(self, char):
@@ -33,5 +63,5 @@ class SevenSegmentGPIO(GPIODevice):
         :type char: str
         :param char: Character to display
         """
-        char = str(char)
+        char = str(char).lower()
         self.segments.write(self.font[char])
